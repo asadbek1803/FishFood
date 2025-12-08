3a525179c9f48dd59e95d4c4ec560d63e49834e8
Asadbek <asadbekoffical1202@gmail.com>
2025-12-08 18:11:13 +0500

    Update: add dotenv

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 from django.urls import reverse_lazy
 
+load_dotenv()  # Load environment variables from .env file
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
