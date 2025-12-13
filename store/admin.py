6444132a536c8f859048e7e0dee2a7cc2eaacc22
Asadbek <asadbekoffical1202@gmail.com>
2025-12-13 17:58:13 +0500

    Fix: No image update

--- store/admin.py
+++ store/admin.py
@@ -68,13 +68,12 @@ class ProductAdmin(UnfoldModelAdmin):
     def display_image(self, obj):
         if not obj.image:
             return format_html(
-                '{}',
                 '<div style="width:50px;height:50px;background:#2C2940;border-radius:5px;'
                 'display:flex;align-items:center;justify-content:center;">'
                 '<i class="fas fa-fish" style="color:#60D5F4;"></i>'
                 '</div>'
             )
-
+        
         return format_html(
             '<img src="{}" width="50" height="50" style="border-radius:5px;object-fit:cover;" />',
             obj.image.url
